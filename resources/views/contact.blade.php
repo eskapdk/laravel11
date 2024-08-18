486c1fa174b889b0ebecae640ccf598588bdf830
Eska <eureka.elwing@gmail.com>
2024-08-18 22:59:47 +0700

    reformat

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
-<x-slot:heading>
-    Halaman Kontak
-</x-slot:heading>
+    <x-slot:heading>
+        Halaman Kontak
+    </x-slot:heading>
     <h1>Halo ini dari halaman Kontak</h1>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
-<x-slot:heading>
-    Halaman Beranda
-</x-slot:heading>
+    <x-slot:heading>
+        Halaman Beranda
+    </x-slot:heading>
     <h1>Halo ini dari halaman home</h1>
 </x-layout>
