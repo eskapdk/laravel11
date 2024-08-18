2c011b096a86a623ec0b2f7cd6f1e2270ca91282
Eska <eureka.elwing@gmail.com>
2024-08-18 22:53:33 +0700

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+<x-slot:heading>
+    Halaman Tentang
+</x-slot:heading>
+    <h1>Halo ini dari halaman Tentang</h1>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+<x-slot:heading>
+    Halaman Beranda
+</x-slot:heading>
+    <h1>Halo ini dari halaman home</h1>
+</x-layout>
